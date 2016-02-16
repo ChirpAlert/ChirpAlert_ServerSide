dd11831f149dd06c25e1bb83a3c5ebd09bc690ef
Jennifer Bennett <jennbbennett@hotmail.com>
2016-02-16 15:31:54 -0700

    notes on db queries

--- routes/db_api.js
+++ routes/db_api.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 var db = require('monk')(process.env.MONGOLAB_URI);
 var bson = require('bson');
 
+
 var users = db.get('users');
 
 users.insert({id: 'id'});
@@ -9,3 +10,14 @@ users.insert({id: 'id'});
 console.log('in the db file');
 
 db.close();
+
+
+//save user => attach twitter id to user document
+
+// save bird to list => {user: "twitterid", bird: {
+// id: "id", name: "English name", loc: "location", time: "time", image: "imageurl", audio: "audiourl"
+// }}
+
+// getbirdList => get all documents associated with twitterid
+
+// delete => delete bird by xeno-canto and twitterid
