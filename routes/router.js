92fa60331730e4d17ab7fc9ad78582b62d012cad
William Johnson <wjohnson1000@gmail.com>
2016-02-16 18:58:24 -0700

    all routes except delete are functioning

--- routes/router.js
+++ routes/router.js
@@ -8,15 +8,30 @@ router.get('/adduser', function(request, response) {
 });
 
 router.get('/savebird', function(request, response) {
-
+  db_api.saveBird(newbird);
+  response.end();
 });
 
 router.get('/getbirdlist', function(request, response) {
-
+  db_api.getBirdList('billy');
+  response.end();
 });
 
 router.get('/deletebird', function(request, response) {
-
+  db_api.deleteBird(newbird);
+  response.end();
 });
+
+var newbird = {
+  twitterId: 'jenn',
+  bird: {
+    id: 3,
+    englishName: 'sparrow',
+    loc: 'denver',
+    timeSaved: 'this morning',
+    imageUrl: 'google it', 
+    audioUrl: 'google it'
+  }
+}
 
 module.exports = router;
